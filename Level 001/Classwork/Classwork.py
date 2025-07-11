bc3f79283e29cb6d2d3514fe4c6546dd0b495914
ninokaxidze <ninokaxidze2015@gmail.com>
2025-07-11 15:55:20 +0400

    commit

---
+++ Level 001/Classwork/Classwork.py
@@ -0,0 +1,6 @@
+name1="nino"
+age1=10
+height=1.45
+print(name1)
+print(age1)
+print(height)
---
+++ Level 001/Homework/homework.py
@@ -0,0 +1,14 @@
+name1="nino"
+age1=10
+year=2015
+pilmi="lilo stich"
+
+
+mom="malvina"
+dad="bidzina"
+sister="mari"
+pet="lusi"
+print(mom)
+print(dad)
+print(sister)
+print(pet)
