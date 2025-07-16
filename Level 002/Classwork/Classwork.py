75363717c7f62b0264e4d2be60be2062b9cbc5e8
ninokaxidze <ninokaxidze2015@gmail.com>
2025-07-16 17:20:49 +0400

    commit

---
+++ Level 002/Classwork/Classwork.py
@@ -0,0 +1,26 @@
+#1
+
+
+#print"hello)
+print("hello")
+
+#5name" = 'Aleksandre"
+#print(5name)
+name5 = "Aleksandre"
+print(name5)
+
+#kibatono = "123'
+#print = kibatono
+kibatono = 123
+print  (kibatono)
+
+#hello* = 123
+#print(hello)
+hello = 123
+print (hello)
+
+
+
+#2
+
+#snake_case და camelCase ორივე არის ცვალდის შექმნის სტილი პროგრამირებაში, რომელშიც თავისუფლად შეგვძლია შევინახოთ გარკვეული ტიპის ინფორმაცია 
---
+++ Level 002/Homework/Homework.py
@@ -0,0 +1,106 @@
+#1
+
+
+#snake_case ში სფეისები ჩანაცვლებულია underscore ბით ანუ ქვედა ტირეებით და camelCase ში პირველი სიტყვის შემდეგ ყოველი მომდევნო სიტყვის პირველი ასო აღინიშნება დიდი ასოთი
+
+
+#2
+
+
+#პითონი მიეკუთვნება case-sensitive ენების კატეგორიას რადგან, პითონში პატარა და დიდ ასოს არანაირი საერთო არ აქვთ, უფრო კონკრეტულად კი, პითონში პატარა ასო სრულიად განსხვავდება დიდი ასოსგან
+
+#3
+
+
+#Debbuging არის პროცესი, როდესაც შენ ცდილობ რომ გაასწორო ერორები ან ბაგები შენს კოდში, რათა შეუწყო ხელი შენ კოდს ხარისხიანად და სწორად მუშაობაში
+
+
+#4
+
+
+#ცვლადის შექმნას არასდროს ვიწყებთ რიცხვით
+
+#ცვლადში არ შეგვიძლია რომ გამოვიყენოთ სპეციალური სიმბოლოები
+
+#ცვლადში არ უნდა გვქონდეს სფეისები, თუ მაინც და მაინც მოგვიწევს სფეისის გაკეთება, უნდა გამოვიყენოთ snake_case
+
+
+#5
+
+
+#კომენტარი აღწერს ჩვენს დაჭერილ კოდს რადგან სხვებს გაუმარტივდეთ ჩვენი კოდის წაკიტხვა ასევე გამოიყენაბა იმისტვის რომ აღვწეროთ რაიმე
+
+
+#6
+
+
+#name -> "Giorgi'
+#print(name)
+
+# luka = "name"
+# print(Luka)
+
+# 2helomynameis = "anamaria"
+# print(helomynameis)
+
+name1 = "Giorgi"
+print (name1)
+
+Luka="name"
+print (Luka)
+
+helomynameis = "anamaria"
+print (helomynameis)
+
+
+#7
+
+
+name1 = "string"
+
+age = 10
+
+boo = True
+
+print (name1)
+
+print (age)
+
+print (boo)
+
+
+#8
+
+
+snake_case = "nino"
+cemelCase = "kaxidze"
+print (snake_case)
+print (cemelCase)
+
+
+#9
+
+
+myName = "nino"
+myAge = 10
+myHeight = 1.45
+myLastName = "kaxidze"
+myCar = "porshe"
+
+my_movie = "squit game" 
+my_color = "blue"
+my_pet = "dog"
+my_pet2 = "prrot"
+my_sister_age = 8
+
+print (myName)
+print (myAge)
+print (myHeight)
+print (myLastName)
+print (myCar)
+print (my_movie)
+print (my_color)
+print (my_pet)
+print (my_pet2)
+print (my_sister_age)
+
